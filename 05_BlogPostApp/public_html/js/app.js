9d8f740bab758a923cc8787614c9dc8bbbfd5b62
Amandeep Chawla <amandeep@adobe.com>
2018-02-23 15:44:59 -0800

    Fixing css issues

--- 05_BlogPostApp/public_html/js/app.js
+++ 05_BlogPostApp/public_html/js/app.js
@@ -12,6 +12,11 @@ blogApp.config([
                 templateUrl: 'partials/main.html',
                 controller: 'BlogCtrl'
             }
+        ).when(
+            '/blogPost/:id', {
+                templateUrl: 'partials/blogPost.html',
+                controller: 'BlogViewCtrl'
+            }
         );
         
         $locationProvider.html5Mode(false).hashPrefix('!');
